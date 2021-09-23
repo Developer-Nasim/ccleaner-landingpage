fc263813e3e569d31fe481b702266f8323929dbb
Developer-Nasim <ajnasim72@gmail.com>
2021-09-23 16:24:21 +0600

    up

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+(function($) {
+  "use strict";
+  
+ // menu 
+  $('.siteBar-btn').click( function (){ 
+    $('.mobile-menu').toggleClass('siteBar');   
+  }); 
+ 
+
+  // owlCarousel
+  // $(".pricing-slider").owlCarousel({
+  //   loop: true,
+  //   margin: 30,
+  //   items: 5, 
+  //   nav: false,
+  //   dots: false,
+  //   center: true,
+  //   responsive: {
+  //     0: {
+  //       items: 1
+  //     },
+  //     767: {
+  //       items: 3
+  //     },
+  //     992: {
+  //       items: 5
+  //     }
+  //   }
+  // });
+ 
+ 
+ 
+})(jQuery);
